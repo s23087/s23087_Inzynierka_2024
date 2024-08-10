114374755f3a38d5b36698fbe2f92b1071a1a913
unknown <s23087@pjwstk.edu.pl>
2024-08-10 19:55:08 +0200

    added new functionality

--- web/handler_b2b/src/app/dashboard/warehouse/page.js
+++ web/handler_b2b/src/app/dashboard/warehouse/page.js
@@ -1,12 +1,33 @@
+"use client";
+
+import { useState } from "react";
 import PagePostionBar from "@/components/menu/page_position_bar";
 import SearchFilterBar from "@/components/menu/search_filter_bar";
+import MenuTemplate from "@/components/menu/menu_template";
+import CurrencyChangeButton from "@/components/smaller_components/currency_change_button";
+import CustomSidebar from "@/components/menu/sidebars/sidebar";
 
-export default function warehousePage() {
+export default function WarehousePage() {
+  const [sidebarShow, setSidebarShow] = useState(false);
+  const showSidebar = () => setSidebarShow(true);
+  const hideSidebar = () => setSidebarShow(false);
+
   return (
     <main>
       <nav>
+        <MenuTemplate
+          sidebar_action={showSidebar}
+        >
+            <CurrencyChangeButton currency="PLN" />
+        </MenuTemplate>
         <PagePostionBar site_name="Warehouse" with_switch="true" />
         <SearchFilterBar filter_icon_bool="false" />
+        <CustomSidebar
+          user="<<user name>>"
+          org_name="<<Org name>>"
+          offcanvasShow={sidebarShow}
+          onHideAction={hideSidebar}
+        />
       </nav>
     </main>
   );
